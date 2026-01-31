47147a39fd30e186c3dbaac33026a6606672bb09
mariambaha <mariam.baha2001@gmail.com>
2026-01-31 06:17:16 +0200

    Add AppDbContext with Users, Tasks, and Logs

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MetaSoftware_TaskManagement.API.Models;
+
+namespace MetaSoftware_TaskManagement.API.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<TaskItem> Tasks { get; set; }
+        public DbSet<Log> Logs { get; set; }
+    }
+}
