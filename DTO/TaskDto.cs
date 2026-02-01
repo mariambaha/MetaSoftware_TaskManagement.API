e8639e11c634b45080883159b1c8192f5849b16a
mariambaha <mariam.baha2001@gmail.com>
2026-02-01 02:12:59 +0200

    Add DTOs

---
+++ DTO/TaskDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MetaSoftware_TaskManagement.API.DTO
+{
+    public class TaskDto
+    {
+        [Required, MaxLength(100)]
+        public string Title { get; set; }
+
+        [MaxLength(500)]
+        public string Description { get; set; }
+    }
+
+}
