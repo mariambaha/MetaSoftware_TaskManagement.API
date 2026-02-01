8d4c37d8ebf3f421d1ab8823fc91e2731f9149b1
mariambaha <mariam.baha2001@gmail.com>
2026-02-01 06:20:38 +0200

    Add JwtMiddleware

---
+++ Middleware/JWT Middleware.cs
@@ -0,0 +1,32 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace MetaSoftware_TaskManagement.API.Middleware
+{
+    public class JwtMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public JwtMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            var token = context.Request.Headers["Authorization"]
+                .FirstOrDefault()?.Split(" ").Last();
+
+            if (token != null)
+            {
+                var handler = new JwtSecurityTokenHandler();
+                var jwt = handler.ReadJwtToken(token);
+                context.Items["UserId"] = jwt.Claims
+                    .First(x => x.Type == ClaimTypes.NameIdentifier).Value;
+            }
+
+            await _next(context);
+        }
+    }
+
+}
