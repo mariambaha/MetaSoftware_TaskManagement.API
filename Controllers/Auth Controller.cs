0592e29b97ac785c5c76a0ca80e787d9fc50c2c5
mariambaha <mariam.baha2001@gmail.com>
2026-02-01 05:06:01 +0200

    authentication endpoints

---
+++ Controllers/Auth Controller.cs
@@ -0,0 +1,55 @@
+﻿using MetaSoftware_TaskManagement.API.Data;
+using MetaSoftware_TaskManagement.API.DTO;
+using MetaSoftware_TaskManagement.API.Models;
+using MetaSoftware_TaskManagement.API.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace MetaSoftware_TaskManagement.API.Controllers
+{
+    
+    [ApiController]
+    [Route("api/auth")]
+    public class AuthController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        private readonly AuthService _authService;
+
+        public AuthController(AppDbContext context, AuthService authService)
+        {
+            _context = context;
+            _authService = authService;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(RegisterDto dto)
+        {
+            if (await _context.Users.AnyAsync(u => u.Email == dto.Email))
+                return BadRequest("Email already exists");
+
+            var user = new User
+            {
+                Email = dto.Email.Trim(),
+                PasswordHash = BCrypt.Net.BCrypt.HashPassword(dto.Password)
+            };
+
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return Ok();
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginDto dto)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.Email == dto.Email);
+            if (user == null || !BCrypt.Net.BCrypt.Verify(dto.Password, user.PasswordHash))
+                return Unauthorized();
+
+            var token = _authService.GenerateToken(user);
+            return Ok(new { token });
+        }
+    }
+
+}
